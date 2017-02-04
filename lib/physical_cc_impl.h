0c33382f2e6b59e90ea63a57f326cf08d2fa9dd5
Ron Economos <w6rz@comcast.net>
2017-02-04 02:49:01 -0800

    Update copyright dates.

--- lib/physical_cc_impl.h
+++ lib/physical_cc_impl.h
@@ -1,6 +1,6 @@
 /* -*- c++ -*- */
 /* 
- * Copyright 2014,2016 Ron Economos.
+ * Copyright 2014,2016,2017 Ron Economos.
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
