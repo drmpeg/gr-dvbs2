e63de58e120331900fa5c72c97a8832423faa272
Ron Economos <w6rz@comcast.net>
2020-07-08 23:53:01 -0700

    Update for GNU Radio 3.8

--- cmake/Modules/dvbs2Config.cmake
+++ cmake/Modules/dvbs2Config.cmake
@@ -22,9 +22,10 @@ FIND_LIBRARY(
           /usr/local/lib64
           /usr/lib
           /usr/lib64
-)
+          )
+
+include("${CMAKE_CURRENT_LIST_DIR}/dvbs2Target.cmake")
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(DVBS2 DEFAULT_MSG DVBS2_LIBRARIES DVBS2_INCLUDE_DIRS)
 MARK_AS_ADVANCED(DVBS2_LIBRARIES DVBS2_INCLUDE_DIRS)
-
