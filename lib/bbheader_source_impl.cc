c2e699ebe2d3e27e32123c94130b80343ce97f9b
Bjoern Kerler <info@revskills.de>
2023-08-01 16:03:45 +0200

    Fix spdlog issues

--- lib/bbheader_source_impl.cc
+++ lib/bbheader_source_impl.cc
@@ -1089,7 +1089,7 @@ namespace gr {
               d_socket->send_to(boost::asio::buffer((void*)(udp_packet), (kbch[i] / 8) + 4), d_endpoint);
             }
             catch(std::exception& e) {
-              GR_LOG_ERROR(d_logger, boost::format("send error: %s") % e.what());
+              d_logger->error("send error: {}",e.what());
             }
           }
         }
